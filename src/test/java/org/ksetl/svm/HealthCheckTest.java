c7600646dfdda20bf1c7eb13f132682cb7a4f726
Stephen Nimmo <stephennimmo@gmail.com>
2022-12-23 08:49:27 -0600

    Added health check and prometheus metrics endpoints

---
+++ src/test/java/org/ksetl/svm/HealthCheckTest.java
@@ -0,0 +1,30 @@
+package org.ksetl.svm;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class HealthCheckTest {
+
+    @Test
+    public void getHealth() {
+        given()
+                .when().get("/q/health")
+                .then()
+                .statusCode(200)
+                .body("status", Matchers.equalTo("UP"));
+    }
+
+    @Test
+    public void getDatasourceHealth() {
+        given()
+                .when().get("/q/health/ready")
+                .then()
+                .statusCode(200)
+                .body("status", Matchers.equalTo("UP"));
+    }
+
+}
